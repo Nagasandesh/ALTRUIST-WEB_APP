01ad75f973fbc05f5766f6cd5d93c5890b0ef752
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-05-18 16:02:50 +0530

    Update Navbar.js

--- Pramod/ALTRUIST/src/components/Navbar.js
+++ Pramod/ALTRUIST/src/components/Navbar.js
@@ -1,27 +1,14 @@
-// import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
 
 
 function Navbar() {
-    // const [sidebar, setSidebar] = useState(false)
-    // const showSidebar = () => setSidebar(!sidebar)
     return (
         <>
             <div className="navbar">
-                {/* <Link to="#" className="menu-bars">
-                    <FaIcons.FaBars onClick={showSidebar}/>
-                </Link> */}
-            
-            
             <nav className='nav-menu'>
                 <ul className="nav-menu-items">
-                    {/* <li className="navbar-toggle">
-                        <Link to="#" className="menu-bars">
-                        <AiIcons.AiOutlineClose/>
-                        </Link>
-                    </li> */}
                     <Link to="./" className="logoSection">
                         <img className="logo" alt="CompanyLogo" src="./images/logo1.jpg"/>
                     </Link>
