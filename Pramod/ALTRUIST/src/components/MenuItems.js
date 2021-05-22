24a1bdf5a9090707c689a10921d24a131435bf98
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-05-22 07:34:58 +0530

    Update and rename SidebarData.js to MenuItems.js

---
+++ Pramod/ALTRUIST/src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+      title: 'Home',
+      path: '/',
+      cName: 'nav-links'
+    },
+    {
+      title: 'Chatlogin',
+      path: '/Chatlogin',
+      cName: 'nav-links'
+    },
+    {
+      title: 'Ngo',
+      path: '/Ngo',
+      cName: 'nav-links'
+    },
+    {
+        title: 'GetInTouch',
+        path: '/GetInTouch',
+        cName: 'nav-links'
+      }
+  ];
