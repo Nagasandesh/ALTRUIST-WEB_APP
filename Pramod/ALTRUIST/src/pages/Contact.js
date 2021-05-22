3a9379e9c9410228b091ecc99b67538fc19cc488
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-05-22 09:50:14 +0530

    Create Contact.js

---
+++ Pramod/ALTRUIST/src/pages/Contact.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className="Contact">
+            <header class="cd-header flex flex-column flex-center">
+                <div class="text-component text-center"></div>
+            </header>
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                            <img src="./images/member1111.jpeg" alt="Sowndarya" width="450" height="300"/>
+                            <div class="container">
+                                <h2>Sowndarya Spoorthi B</h2>
+                                <p>soundaryaspoorthi04.ss@gmail.com</p>
+                                <a href="https://www.linkedin.com/in/sowndarya-spoorthi-296909163">
+                                    <img src="./images/lkin.jpeg" width="200" height="70"/>
+                                </a>
+                    </div>
+                </div>
+
+            
+                <div class="column">
+                    <div class="card">
+                        <img src="./images/member2.jpeg" alt="Harshita" width="450" height="300"/>
+                        <div class="container">
+                            <h2>Harshitha V</h2>
+                            <p>harshithavgowda2000@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/harshitha-v-4034a81b3">
+                                <img src="./images/lkin.jpeg" width="200" height="70"/></a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="column">
+                    <div class="card">
+                        <img src="./images/member3.jpeg" alt="Shivani" width="450" height="300"/>
+                        <div class="container">
+                            <h2>Shivani M N</h2>
+                            <p>shivanimn84@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/shivani-m-n-020203190"><img src="./images/lkin.jpeg" width="200" height="70"/></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <img src="./images/member44.jpeg" alt="Nagasandesh" width="450" height="300"/>
+                        <div class="container">
+                            <h2>Nagasandesh N</h2>
+                            <p>sandeshcs09@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/nagasandesh-n-808997195">
+                                <img src="./images/lkin.jpeg" width="200" height="70"/></a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="column">
+                    <div class="card">
+                        <img src="./images/member55.jpeg" class="alldevice" alt="Raksha" width="450" height="300"/>
+                        <div class="container">
+                            <h2>Shree Raksha G M</h2>
+                            <p>rakshaiyer1234@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/raksha-iyer-b0b6641a5">
+                                <img src="./images/lkin.jpeg" width="200" height="70"/></a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="column">
+                    <div class="card">
+                        <img src="./images/member6.jpg" alt="Pramod" width="450" height="300"/>
+                        <div class="container">
+                            <h2>K Sathya Pramod</h2>
+                            <p>sathyapramod7@gmail.com</p>
+                            <a href="https://www.linkedin.com/in/sathya-pramod-k-717841185">
+                                <img src="./images/lkin.jpeg" width="200" height="70"/></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default Contact
